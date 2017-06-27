2fb315c1c87f91f24ff08e32bf327111af32a4c6
ShogoFunaguchi <gooodsun1@gmail.com>
2017-06-27 17:43:57 +0900

    add view

---
+++ HelpNagoyaSpeciality/HelpNagoyaSpeciality/GameScene.swift
@@ -0,0 +1,9 @@
+//
+//  GameScene.swift
+//  HelpNagoyaSpeciality
+//
+//  Created by Shogo Funaguchi on 2017/06/23.
+//  Copyright © 2017年 Shogo Funaguchi. All rights reserved.
+//
+
+import Foundation
