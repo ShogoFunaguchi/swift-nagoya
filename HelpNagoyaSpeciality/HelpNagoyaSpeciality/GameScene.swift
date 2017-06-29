c7a9fa5a3a694204657438c0976f88b7f89d1b2b
ShogoFunaguchi <gooodsun1@gmail.com>
2017-06-29 15:28:02 +0900

    add donburi

--- HelpNagoyaSpeciality/HelpNagoyaSpeciality/GameScene.swift
+++ HelpNagoyaSpeciality/HelpNagoyaSpeciality/GameScene.swift
@@ -10,22 +10,50 @@ import Foundation
 import SpriteKit
 
 class GameScene : SKScene {
+    
+    
+    // 丼
+    var bowl:SKSpriteNode?
+    
+    
     override func didMove(to view: SKView) {
         
+        //重力
         self.physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
         
         
-        
+        //バックグランドイメージ
         let background = SKSpriteNode(imageNamed: "background")
         
         background.position = CGPoint(x: self.size.width*0.5, y: self.size.height*0.5)
         background.size = self.size
         self.addChild(background)
         
+        
+        // どんぶり
+        let bowlTexture = SKTexture(imageNamed: "bowl")
+        let bowl = SKSpriteNode(texture: bowlTexture)
+        bowl.position = CGPoint(x: self.size.width*0.5,y: 100)
+        bowl.size = CGSize(width: bowlTexture.size().width*0.5, height: bowlTexture.size().height*0.5)
+        
+        bowl.physicsBody = SKPhysicsBody(texture: bowlTexture, size: bowl.size)
+        bowl.physicsBody?.isDynamic = false
+        
+        self.bowl = bowl
+        self.addChild(bowl)
+        
         self.fallNagoyaSpeciality()
     }
     
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: <#T##SKNode#>)
+        }
+    }
+    
     func fallNagoyaSpeciality() {
+        
         let texture = SKTexture(imageNamed: "0")
         let sprite = SKSpriteNode(texture: texture)
         sprite.position = CGPoint(x:self.size.width*0.5, y:self.size.height*0.5)
@@ -34,8 +62,9 @@ class GameScene : SKScene {
         sprite.physicsBody = SKPhysicsBody(texture: texture, size: sprite.size)
         
         self.addChild(sprite)
-        
-        
+
     }
+    
+    
     
 }
